85d2530862dc97d5d397e9a43b3c175e596091bb
Lee Seo Yeon <elvina0902@naver.com>
2019-09-19 15:47:11 +0900

    Create Seat.cs

---
+++ Cake/StarBucks.Core/Seat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarBucks.Core
+{
+    public class Seat
+    {
+        public int Id { get; set; }
+
+        public List<Drink> lstDrink = new List<Drink>();
+
+        public string OrderTime { get; set; }
+    }
+}
