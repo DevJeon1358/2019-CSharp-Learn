ea3769ad6b35071a05c19ca37289cea85d6f9edb
Jeon.G.Y <square.jeon@gmail.com>
2019-09-19 13:40:58 +0900

    Init Project

--- Cake/StarBucks.Analytics/Class1.cs
+++ Cake/StarBucks.Analytics/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StarBucks.Core
+namespace StarBucks.Analytics
 {
     public class Class1
     {
--- Cake/StarBucks.Analytics/Properties/AssemblyInfo.cs
+++ Cake/StarBucks.Analytics/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StarBucks.Core")]
+[assembly: AssemblyTitle("StarBucks.Analytics")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("StarBucks.Core")]
+[assembly: AssemblyProduct("StarBucks.Analytics")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8541d9f7-217c-4be4-a189-5bcc00646c6b")]
+[assembly: Guid("50189801-84f2-44a7-930d-3a8bee57d26b")]
 
 // Version information for an assembly consists of the following four values:
 //
