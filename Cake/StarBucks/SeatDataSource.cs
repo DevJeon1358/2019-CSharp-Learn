c2eccf51e96876e239c15343586ffe6ec4938b22
Lee Seo Yeon <elvina0902@naver.com>
2019-09-19 15:49:11 +0900

    Create SeatDataSource.cs

---
+++ Cake/StarBucks/SeatDataSource.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StarBucks.Core;
+
+namespace StarBucks
+{
+    public class SeatDataSource
+    {
+        bool isLoaded = false;
+        public List<Seat> lstSeat = null;
+
+        public void Load()
+        {
+            if (isLoaded) return;
+
+            lstSeat = new List<Seat>()
+            {
+                new Seat() {Id = 1},
+                new Seat() {Id = 2},
+                new Seat() {Id = 3},
+                new Seat() {Id = 4},
+                new Seat() {Id = 5},
+                new Seat() {Id = 6}
+
+            };
+
+            isLoaded = true;
+        }
+    }
+}
