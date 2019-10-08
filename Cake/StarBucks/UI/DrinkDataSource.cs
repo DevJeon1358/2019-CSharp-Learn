49634e845a1df5e3260688d6ca1a2f272232e605
ChoiSeokJun <ghdudwlwns@gmail.com>
2019-10-08 14:08:29 +0900

    [FIX] DrinkDataSource
    이미지 url 수정

--- Cake/StarBucks/UI/DrinkDataSource.cs
+++ Cake/StarBucks/UI/DrinkDataSource.cs
@@ -16,17 +16,17 @@ namespace StarBucks
         {
             listDrink = new List<Drink>()
             {
-                new Drink() {Name = "나이트로 콜드 브루", Price = 5500, ImagePath = @"UI/Assets/콜드브루/나이트로 콜드 브루.jpg"},
-                new Drink() {Name = "돌체 콜드 브루", Price = 6000, ImagePath = @"UI/Assets/콜드브루/돌체 콜드 브루.jpg"},
-                new Drink() {Name = "바닐라 크림 콜드 브루", Price = 5500, ImagePath = @"UI/Assets/콜드브루/바닐라 크림 콜드 브루.jpg"},
+                new Drink() {Name = "나이트로 콜드 브루", Price = 5500, ImagePath = @"Assets/ColdBrew/나이트로 콜드 브루.jpg"},
+                new Drink() {Name = "돌체 콜드 브루", Price = 6000, ImagePath = @"Assets/ColdBrew/돌체 콜드 브루.jpg"},
+                new Drink() {Name = "바닐라 크림 콜드 브루", Price = 5500, ImagePath = @"Assets/ColdBrew/바닐라 크림 콜드 브루.jpg"},
 
-                new Drink() {Name = "라벤더 카페 브레베", Price = 5500, ImagePath = @"UI/Assets/에스프레소/라벤더 카페 브레베.jpg"},
-                new Drink() {Name = "블론드 에스프레소 토닉", Price = 6000, ImagePath = @"UI/Assets/에스프레소/블론드 에스프레소 토닉.jpg"},
-                new Drink() {Name = "아이스 블랙 글레이즈드 라떼", Price = 5500, ImagePath = @"UI/Assets/에스프레소/아이스 블랙 글레이즈드 라떼.jpg"},
+                new Drink() {Name = "라벤더 카페 브레베", Price = 5500, ImagePath = @"Assets/Espresso/라벤더 카페 브레베.jpg"},
+                new Drink() {Name = "블론드 에스프레소 토닉", Price = 6000, ImagePath = @"Assets/Espresso/블론드 에스프레소 토닉.jpg"},
+                new Drink() {Name = "아이스 블랙 글레이즈드 라떼", Price = 5500, ImagePath = @"Assets/Espresso/아이스 블랙 글레이즈드 라떼.jpg"},
 
-                new Drink() {Name = "제주 쑥떡 크림 프라푸치노", Price = 5500, ImagePath = @"UI/Assets/프라푸치노/제주 쑥떡 크림 프라푸치노.jpg"},
-                new Drink() {Name = "초콜릿 크림 프라푸치노", Price = 6000, ImagePath = @"UI/Assets/프라푸치노/초콜릿 크림 프라푸치노.jpg"},
-                new Drink() {Name = "화이트 딸기 크림 프라푸치노", Price = 5500, ImagePath = @"UI/Assets/프라푸치노/화이트 딸기 크림 프라푸치노.jpg"},
+                new Drink() {Name = "제주 쑥떡 크림 프라푸치노", Price = 5500, ImagePath = @"Assets/Frappuccino/제주 쑥떡 크림 프라푸치노.jpg"},
+                new Drink() {Name = "초콜릿 크림 프라푸치노", Price = 6000, ImagePath = @"Assets/Frappuccino/초콜릿 크림 프라푸치노.jpg"},
+                new Drink() {Name = "화이트 딸기 크림 프라푸치노", Price = 5500, ImagePath = @"Assets/Frappuccino/화이트 딸기 크림 프라푸치노.jpg"},
             };
             for (int i = 0; i < 3; i++)
             {
@@ -45,6 +45,9 @@ namespace StarBucks
         }
         public void set(string a)
         {
+            if (listDrink == null)
+                return;
+
             //listTest.Clear();
             listTest = listDrink.Where(x => x.Category.Equals(a)).ToList();
         }
