e4f378ebb99fa263717b23c94907f588c1ea2313
Lee Seo Yeon <elvina0902@naver.com>
2019-10-22 15:37:53 +0900

    Update Main.xaml.cs
    빠진거...

--- Cake/StarBucks/UI/Main.xaml.cs
+++ Cake/StarBucks/UI/Main.xaml.cs
@@ -99,7 +99,9 @@ namespace StarBucks.UI
             orderControl.Visibility = Visibility.Visible;
 
             // Table 번호
-            orderControl.tableIdx = 0;
+            orderControl.tableIdx = id;
+            var item = App.SeatData.lstSeat.Where(x => x.Id == id).FirstOrDefault();
+            orderControl.setOrderList(item.lstDrink);
 
             lstSeat.SelectedIndex = -1;
         }
