2af168a8a9333144b19c4f3179f81dd9a7cb3eb3
Jeon.G.Y <square.jeon@gmail.com>
2019-11-14 15:47:50 +0900

    Remove UnUsed Using

--- Cake/StarBucks/UI/OrderControl.xaml.cs
+++ Cake/StarBucks/UI/OrderControl.xaml.cs
@@ -3,16 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using StarBucks.Analytics;
 using System;
-using StarBucks.UI;
 
 namespace StarBucks
 {
